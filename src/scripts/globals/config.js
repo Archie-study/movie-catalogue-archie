56a76932be5f9e26749e0ce8bf07e9776476d6df
archiestudy12@gmail.com <archiestudy12@gmail.com>
2023-11-12 02:45:23 +0700

    Commit Keenam - Melakukan Request dari API

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: '26fe1081d1aef840d1f5743f207a3aa9',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500',
+    DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
