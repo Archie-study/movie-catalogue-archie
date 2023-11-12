45fb19f01db6787068ad248bf38b6da88f2dac4d
archiestudy12@gmail.com <archiestudy12@gmail.com>
2023-11-13 00:15:03 +0700

    Commit Kedua Belas - Offline Capability dengan Workbox

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,18 +1,4 @@
-// const swRegister = async () => {
-//     if (!('serviceWorker' in navigator)) {
-//         console.log('Service Worker not supported in the browser');
-//         return;
-//     }
-
-//     try {
-//         await navigator.serviceWorker.register('./sw.bundle.js');
-//         console.log('Service worker registered');
-//     } catch (error) {
-//         console.log('Failed to register service worker', error);
-//     }
-// };
-
-// export default swRegister;
+import { Workbox } from 'workbox-window';
 
 const swRegister = async () => {
     if (!('serviceWorker' in navigator)) {
@@ -20,8 +6,10 @@ const swRegister = async () => {
         return;
     }
 
+    const wb = new Workbox('./sw.bundle.js');
+
     try {
-        await navigator.serviceWorker.register('./sw.bundle.js');
+        await wb.register();
         console.log('Service worker registered');
     } catch (error) {
         console.log('Failed to register service worker', error);
